65e1f21f13102b076156e2e6a07486c06f5b25d1
Vitaly Buka <vitalybuka@google.com>
2015-11-05 15:54:05 -0800

    Fix behavior of bootstrap manager after monitoring timeout was reached
    StartMonitoring resets monitor_until_ member always.
    ContinueMonitoring resets monitor_until_ only for online state as we
    don't have timeout in this state.
    
    Removed check for last_configured_ssid.empty() from
    UpdateConnectionState because we should shutdown bootstrapping if device
    is connected using other interface.
    
    BUG:25463084
    
    Change-Id: I0afd943f4a3ca797b65a51236103ea3d345828d2
    Reviewed-on: https://weave-review.googlesource.com/1473
    Reviewed-by: Paul Westbrook <pwestbro@google.com>

--- src/privet/wifi_bootstrap_manager.h
+++ src/privet/wifi_bootstrap_manager.h
@@ -74,6 +74,7 @@ class WifiBootstrapManager : public WifiDelegate {
   void EndConnecting();
 
   void StartMonitoring(const base::TimeDelta& timeout);
+  void ContinueMonitoring(const base::TimeDelta& timeout);
   void EndMonitoring();
 
   // Update the current state, post tasks to notify listeners accordingly to
