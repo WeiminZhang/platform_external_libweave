70712809e243dc768bef4f9f294ad1bf94f3b607
Jacob Marble <jacobmarble@google.com>
2016-02-05 13:28:15 -0800

    Fix some libevhtp usage details.
    Path improperly fetched from request object.
    Bump libevhtp version in order to work around
    https://github.com/ellzey/libevhtp/issues/167
    
    Change-Id: Ic53b72c7eea5c3480537f3c69c73b908940726dd
    Bug: 26986898
    Reviewed-on: https://weave-review.googlesource.com/2512
    Reviewed-by: Vitaly Buka <vitalybuka@google.com>

--- third_party/get_libevhtp.sh
+++ third_party/get_libevhtp.sh
@@ -9,17 +9,19 @@
 cd $(dirname "$0")
 THIRD_PARTY=$(pwd)
 
+LIBEVHTP_VERSION=1.2.11n
+
 mkdir -p include lib
 
 rm -rf $THIRD_PARTY/libevhtp
-curl -L https://github.com/ellzey/libevhtp/archive/1.2.10.tar.gz | tar xz || exit 1
-mv libevhtp-1.2.10 $THIRD_PARTY/libevhtp || exit 1
+curl -L https://github.com/ellzey/libevhtp/archive/$LIBEVHTP_VERSION.tar.gz | tar xz || exit 1
+mv libevhtp-$LIBEVHTP_VERSION $THIRD_PARTY/libevhtp || exit 1
 cd $THIRD_PARTY/libevhtp || exit 1
 
 cmake -D EVHTP_DISABLE_REGEX:BOOL=ON . || exit 1
 make evhtp || exit 1
 
-cp -rf evhtp-config.h evhtp.h evthr/evthr.h htparse/htparse.h $THIRD_PARTY/include/ || exit 1
+cp -rf *.h $THIRD_PARTY/include/ || exit 1
 cp -f libevhtp.a $THIRD_PARTY/lib/ || exit 1
 
 rm -rf $THIRD_PARTY/libevhtp
